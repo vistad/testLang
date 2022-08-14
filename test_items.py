a77648c6bfe4294cdbceea8355c58f804253b8d0
Victor Stadnichenko <vstadnichenko@gmail.com>
2022-08-14 14:42:48 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,15 +1,14 @@
 # pytest -v -s --language=es test_items.py
-# Expected value: Añadir al carrito
 # pytest -v -s --language=fr test_items.py
-# Expected value: Ajouter au panier
 
 from selenium.webdriver.common.by import By
 import time
+from selenium.common.exceptions import WebDriverException
 
 def test_button_present(browser):
-    link = f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed(), \
-    "The button Add to cart is missing"
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed(), "The button Add to cart is missing"
 
+#    assert browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket').is_enabled(), "The button Add to cart is missing"
